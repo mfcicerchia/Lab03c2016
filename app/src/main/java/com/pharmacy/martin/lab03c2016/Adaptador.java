27508d967a530305b29c3799b593ab117c6b6921
Pablo Paletto <pablo.paletto@gmail.com>
2016-09-12 19:29:07 -0300

    ListView implementado.

--- app/src/main/java/com/pharmacy/martin/lab03c2016/Adaptador.java
+++ app/src/main/java/com/pharmacy/martin/lab03c2016/Adaptador.java
@@ -1,32 +1,46 @@
 package com.pharmacy.martin.lab03c2016;
 
 import android.content.Context;
+import android.graphics.Typeface;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.CheckBox;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.text.Format;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
+
+import static android.graphics.Typeface.BOLD;
 
 /**
  * Created by Martin on 08/09/2016.
  */
-public class Adaptador extends BaseAdapter {
+public class Adaptador extends BaseAdapter{
     /*Inflater*/
+    ArrayList<Trabajo> items = new ArrayList<Trabajo>();
+    Context context;
     LayoutInflater inflater;
-    Adaptador(Context context ,List<Trabajo> items) {
+    Adaptador(Context context ,ArrayList<Trabajo> items) {
+        this.items = items;
+        this.context = context;
         inflater = LayoutInflater.from(context);
     }
     @Override
     public int getCount() {
-        return 0;
+        return items.size();
     }
 
     @Override
-    public Object getItem(int position) {
-        return null;
+    public Trabajo getItem(int position) {
+
+        return items.get(position);
+
     }
 
     @Override
@@ -41,12 +55,48 @@ public class Adaptador extends BaseAdapter {
             fila = inflater.inflate(R.layout.oferta_laboral, parent, false);
         }
         ViewHolder holder = (ViewHolder)fila.getTag();
-        if(fila==null){
+        if(holder==null){
             holder=new ViewHolder(fila);
             fila.setTag(holder);
         }
-        //TODO: Hacer el getItem
-        // holder.categoria.setText(this.getItem(position).get);
+
+        Format formatter = new SimpleDateFormat("dd/MM/yyyy", Locale.getDefault());
+
+        holder.categoria.setText(this.getItem(position).getCategoria().getDescripcion());
+        holder.trabajo.setText(this.getItem(position).getDescripcion());
+        holder.trabajo.setTypeface(null, BOLD);
+        holder.horas.setText(String.format(Locale.getDefault(),"%1$d ",this.getItem(position).getHorasPresupuestadas()));
+        holder.precioHora.setText(String.format(Locale.getDefault(),"%1$.2f ",this.getItem(position).getPrecioMaximoHora()));
+        holder.fecha.setText(formatter.format(this.getItem(position).getFechaEntrega()));
+        switch (this.getItem(position).getMonedaPago()){
+            case 1:
+                holder.bandera.setImageResource(R.drawable.us);
+                holder.moneda.setText(R.string.monedaUS);
+                break;
+            case 2:
+                holder.bandera.setImageResource(R.drawable.eu);
+                holder.moneda.setText(R.string.monedaUE);
+                break;
+            case 3:
+                holder.bandera.setImageResource(R.drawable.ar);
+                holder.moneda.setText(R.string.monedaAR);
+                break;
+            case 4:
+                holder.bandera.setImageResource(R.drawable.uk);
+                holder.moneda.setText(R.string.monedaUK);
+                break;
+            case 5:
+                holder.bandera.setImageResource(R.drawable.br);
+                holder.moneda.setText(R.string.monedaBR);
+                break;
+        }
+        if(this.getItem(position).getRequiereIngles()){
+            holder.ingles.setChecked(true);
+        }
+        else{
+            holder.ingles.setChecked(false);
+        }
+
         return (fila);
     }
 
@@ -58,6 +108,7 @@ public class Adaptador extends BaseAdapter {
         ImageView bandera = null;
         TextView fecha = null;
         TextView moneda = null;
+        CheckBox ingles = null;
 
         ViewHolder(View base){
             this.categoria = (TextView)base.findViewById(R.id.tvCategoria);
@@ -67,6 +118,7 @@ public class Adaptador extends BaseAdapter {
             this.precioHora = (TextView)base.findViewById(R.id.tvPrecioHoraDat);
             this.bandera = (ImageView)base.findViewById(R.id.ivBandera);
             this.fecha = (TextView)base.findViewById(R.id.tvFechaFinDat);
+            this.ingles = (CheckBox)base.findViewById(R.id.cbIngles);
         }
     }
 }
--- app/src/main/java/com/pharmacy/martin/lab03c2016/MainActivity.java
+++ app/src/main/java/com/pharmacy/martin/lab03c2016/MainActivity.java
@@ -1,13 +1,31 @@
 package com.pharmacy.martin.lab03c2016;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView ofertaLaboral;
+    Context context = MainActivity.this;
+    ArrayList<Trabajo> lista = new ArrayList<Trabajo>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setLista(lista);
+        ofertaLaboral = (ListView) findViewById(R.id.lvOfertaLaboral);
+        ofertaLaboral.setAdapter(new Adaptador(context,lista));
+    }
+
+    public void setLista(ArrayList<Trabajo> lista){
+        Trabajo trabajo = new Trabajo();
+        for (int i=0; i<18;i++){
+            lista.add(trabajo.TRABAJOS_MOCK[i]);
+        }
     }
 }
